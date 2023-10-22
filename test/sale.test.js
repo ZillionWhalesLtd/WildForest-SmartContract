d8ddd970d60b4c86ddd5736ebcfb7544bc4b9dbc
Denis <denis@slatestudio.com>
2023-10-22 13:22:10 +0300

    Minor test adjusments

--- test/sale.test.js
+++ test/sale.test.js
@@ -7,7 +7,7 @@ chai.use(deep_equal)
 const { expect } = chai
 
 const cardsContractName = 'WildForestCards'
-const cardsContractSymbol = `WHC`
+const cardsContractSymbol = `WFS`
 const baseTokenURI = 'https://localhost:3000/nfts/'
 const initialPrice = 20
 const initialSupply = 3
